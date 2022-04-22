c26ed3d302d1f4fe2790777856c09820072542a3
yoyo7373 <yousefgomaa@hotmail.com>
2022-04-21 19:33:21 -0700

    building amazon scraper

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,33 @@
 import * as functions from "firebase-functions";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+initializeApp();
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const db = getFirestore();
+const productsColl = db.collection("products");
+exports.runScraper = functions.pubsub
+  .schedule("every 720 minutes")
+  .onRun(async (context) => {
+    await Promise.all([scrapeAmazon()]);
+    return null;
+  });
+const scrapeAmazon = async () => {
+  const allProducts = await productsColl.get();
+  allProducts.forEach(({ doc }: { doc: { data: Function } }) => {
+    const { name } = doc.data();
+    const nameWords = name.split(" ");
+    request(
+      `https://www.amazon.com/s?k=${nameWords.reduce(() => {}, "")}?tag=`,
+      (error: string, response: { statusCode: number }, html: string) => {
+        if (!error && response.statusCode === 200) {
+          const $ = cheerio.load(html);
+        }
+      }
+    );
+  });
+};
